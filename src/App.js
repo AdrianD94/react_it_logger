d4fd6fcbb3e6342ac6ccba84707a9a56b8c78c4b
unknown <ady.drozman2007@gmail.com>
2019-07-04 16:49:36 +0300

    add logs,log items,preloader

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React,{useEffect} from 'react';
+import React,{useEffect,Fragment} from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js'
+import SearchBar from './components/Layout/SearchBar';
+import Logs from './components/Logs/Logs';
 
 import './App.css';
 
@@ -9,9 +11,10 @@ const App=() =>{
     M.AutoInit();
   })
   return (
-    <div className="App">
-      My App
-    </div>
+   <Fragment>
+     <SearchBar/>
+     <div className="container"> <Logs/></div>
+   </Fragment>
   );
 }
 
