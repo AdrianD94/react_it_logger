5a7280faaf2f4ebc188b163f72d42918ce4a6f9c
AdrianD94 <ady.drozman2007@gmail.com>
2019-07-06 20:13:45 +0300

    finish ui

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import React,{useEffect,Fragment} from 'react';
+import React, { useEffect, Fragment } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
-import M from 'materialize-css/dist/js/materialize.min.js'
+import M from 'materialize-css/dist/js/materialize.min.js';
 import SearchBar from './components/Layout/SearchBar';
 import Logs from './components/Logs/Logs';
 import AddBtn from './components/Layout/AddBtn';
-import AddLogModal from './components/Logs/AddLogModal'
+import AddLogModal from './components/Logs/AddLogModal';
+import EditLogModal from './components/Logs/EditLogModal';
+import AddTechModal from './components/Techs/AddTechModal';
+import TechListModal from './components/Techs/TechListModal';
 
 import './App.css';
 
-const App=() =>{
-  useEffect(()=>{
+const App = () => {
+  useEffect(() => {
     M.AutoInit();
-  })
+  });
   return (
-   <Fragment>
-     <SearchBar/>
-     <div className="container"> 
-     <AddBtn/>
-     <AddLogModal/>
-     <Logs/>
-     
-     </div>
-   </Fragment>
+    <Fragment>
+      <SearchBar />
+      <div className='container'>
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <Logs />
+        <AddTechModal/>
+        <TechListModal/>
+      </div>
+    </Fragment>
   );
-}
+};
 
 export default App;
--- src/components/Layout/AddBtn.js
+++ src/components/Layout/AddBtn.js
@@ -8,7 +8,7 @@ const AddBtn = () => {
             </a>
             <ul>
                 <li><a href="#tech-list-modal" className="btn-floating green modal-trigger"><i className="material-icons">person</i></a></li>
-                <li><a href="#tech-modal" className="btn-floating red modal-trigger"><i className="material-icons">person_add</i></a></li>
+                <li><a href="#add-tech-modal" className="btn-floating red modal-trigger"><i className="material-icons">person_add</i></a></li>
             </ul>
             
         </div>
